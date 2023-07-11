aa842c27aa9c0c4e31f9c09c71cd585976780f8d
ruslanvgu <ruslanvgu@mail.ru>
2023-07-11 21:50:20 +0300

    dfs iteration

--- src/graph.cpp
+++ src/graph.cpp
@@ -3,7 +3,8 @@
 void Graph::print()
 {
     using namespace std;
-    for(auto [key, values] : graph_){
+    for(auto [key, values] : graph_)
+    {
         cout << key << " -> ";
         for(auto value : values){
             cout << value <<" ";
@@ -26,26 +27,28 @@ std::vector<int> Graph::dfs_iterations(size_t startVertix)
     std::vector<int> result;
     std::stack<int> stack;
 
-    result.push_back(startVertix);
     stack.push(startVertix);
-    visited[startVertix] =true;
 
-    for(auto v : graph_[startVertix]){
-        if(visited[v]){
-            stack.pop();
-            startVertix = stack.top();
+    while(!stack.empty()){
+        int node = stack.top();
+        stack.pop();
+
+        if(!visited[node]){
+            result.push_back(node);
+            visited[node] = true;
+            for(auto neighbor  : graph_[node]){
+                stack.push(neighbor);
+            }
+
         }
-        result.push_back(v);
-        stack.push(v);
-        visited[v] =true;
-        startVertix = v;
-
     }
+
     return result;
 }
 
 void Graph::dfs_recursion_(size_t startVertix, std::vector<int> &visitedVertixs, std::vector<int> &result)
-{/*
+{
+    /*
     result.push_back(startVertix);
     visitedVertixs[startVertix] = true;
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     Graph graph;
-    graph.addNode(1,{2});
+    graph.addNode(1,{2,3,5});
     graph.addNode(2,{1,3,4});
-    graph.addNode(3,{2,4});
-    graph.addNode(4,{2,3,5});
-    graph.addNode(5,{4});
+    graph.addNode(3,{1,2,3,4,5});
+    graph.addNode(4,{2,3});
+    graph.addNode(5,{1,3});
     graph.print();
     cout << "dfs" <<endl;
     vector<int> vs = graph.dfs_iterations(5);
